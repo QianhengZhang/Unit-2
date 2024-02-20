c24d238754a42e4bf77bf5660b534b38d15acbb3
QianhengZhang <hazeland3427@gmail.com>
2024-02-20 16:49:08 -0600

    finished adaptedTutorial

--- js/adaptedTutorial.js
+++ js/adaptedTutorial.js
@@ -16,6 +16,17 @@ function createMap(){
     getData();
 };
 
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
 
 function getData() {
     fetch("data/MegaCities.geojson")
@@ -23,9 +34,26 @@ function getData() {
             return response.json();
         })
         .then(function(json){
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(json).addTo(map);
-    })
+          //create marker options
+        //     var geojsonMarkerOptions = {
+        //         radius: 8,
+        //         fillColor: "#ff7800",
+        //         color: "#000",
+        //         weight: 1,
+        //         opacity: 1,
+        //         fillOpacity: 0.8
+        //     };
+        //   //create a Leaflet GeoJSON layer and add it to the map
+        //     L.geoJson(json, {
+        //         pointToLayer: function (feature, latlng){
+        //             return L.circleMarker(latlng, geojsonMarkerOptions);
+        //         }
+        //     }).addTo(map);
+        L.geoJson(json, {
+            onEachFeature: onEachFeature
+        }).addTo(map);
+    });
 }
+
 
 document.addEventListener('DOMContentLoaded',createMap)
