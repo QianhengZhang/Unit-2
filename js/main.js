94c33bc19aaf6e773dd54ce9601a12864df065d8
QianhengZhang <hazeland3427@gmail.com>
2024-02-27 14:13:23 -0600

    Activity 6

--- js/main.js
+++ js/main.js
@@ -40,9 +40,8 @@ function calcMinValue(data){
     return minValue;
 }
 
-//calculate the radius of each proportional symbol
 function calcPropRadius(attValue) {
-    //constant factor adjusts symbol sizes evenly
+     //0.15 for my data
     var minRadius = 0.15;
     //Flannery Appearance Compensation formula
     var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
@@ -50,10 +49,7 @@ function calcPropRadius(attValue) {
     return radius;
 };
 
-//Step 3: Add circle markers for point features to the map
 function createPropSymbols(data, attributes){
-
-    //Step 4: Determine which attribute to visualize with proportional symbols
     L.geoJson(data, {
         pointToLayer: function(feature, latlng){
             return pointToLayer(feature, latlng, attributes)
@@ -62,10 +58,8 @@ function createPropSymbols(data, attributes){
 };
 
 function pointToLayer(feature, latlng, attributes) {
-    //console.log(attributes)
     var attribute = attributes[0];
     console.log(attribute)
-    //create marker options
     var geojsonMarkerOptions = {
         fillColor: "#ff7800",
         color: "#fff",
@@ -77,47 +71,38 @@ function pointToLayer(feature, latlng, attributes) {
     var attValue = Number(feature.properties[attribute]);
 
     geojsonMarkerOptions.radius = calcPropRadius(attValue);
-
     var layer = L.circleMarker(latlng, geojsonMarkerOptions);
-    var year = attribute
+    var year = attribute;
     var popupContent = "<p><b>Country:</b> " + feature.properties['Country Name'] + "</p>"
     popupContent += "<p><b>Food Export in " + year + ":</b> " + feature.properties[attribute] + " % of Merchandise Exports</p>";
-
-            //bind the popup to the circle marker
     layer.bindPopup(popupContent, {
         offset: new L.Point(0,-geojsonMarkerOptions.radius)
     });
-            //create circle markers
     return layer
 }
 
 function createSequenceControls(attributes){
-    //create range input element (slider)
+    // slider
     var slider = "<input class='range-slider' type='range'></input>";
     document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
     document.querySelector(".range-slider").max = 10;
     document.querySelector(".range-slider").min = 0;
     document.querySelector(".range-slider").value = 0;
     document.querySelector(".range-slider").step = 1;
-
+    // panel
     document.querySelector('#panel').insertAdjacentHTML('beforeend',"<img src='img/left.png' id='reverse' class='step'>")
     document.querySelector('#panel').insertAdjacentHTML('beforeend',"<img src='img/right.png' id='forward' class='step'>")
+    // add function
     document.querySelectorAll('.step').forEach(function(step){
         step.addEventListener("click", function(){
             var index = document.querySelector('.range-slider').value;
-
-            //Step 6: increment or decrement depending on button clicked
             if (step.id == 'forward'){
                 index++;
-                //Step 7: if past the last attribute, wrap around to first attribute
                 index = index > 10 ? 0 : index;
             } else if (step.id == 'reverse'){
                 index--;
-                //Step 7: if past the first attribute, wrap around to last attribute
                 index = index < 0 ? 10 : index;
             };
-
-            //Step 8: update slider
             document.querySelector('.range-slider').value = index;
             console.log(index);
             updatePropSymbols(attributes[index]);
@@ -125,7 +110,6 @@ function createSequenceControls(attributes){
 
     })
     document.querySelector('.range-slider').addEventListener('input', function(){
-        //Step 6: get the new index value
         var index = this.value;
         console.log(index)
         updatePropSymbols(attributes[index]);
@@ -140,54 +124,36 @@ function updatePropSymbols(attribute){
             //update each feature's radius based on new attribute values
             var radius = calcPropRadius(props[attribute]);
             layer.setRadius(radius);
-
-            //add city to popup content string
             var popupContent = "<p><b>Country:</b> " + props['Country Name'] + "</p>";
-
-            //add formatted attribute to panel content string
             var year = attribute;
             popupContent += "<p><b>Food Export in " + year + ":</b> " + props[attribute] + " % of Merchandise Exports</p>";
-
-            //update popup content
             popup = layer.getPopup();
             popup.setContent(popupContent).update();
         };
     });
 };
 
-//Step 2: Import GeoJSON data
 function getData(){
-    //load the data
     fetch("data/FoodExport.geojson")
         .then(function(response){
             return response.json();
         })
         .then(function(json){
-            //calculate minimum data value
             var attributes = processData(json);
             minValue = calcMinValue(json);
-            //call function to create proportional symbols
             createPropSymbols(json, attributes);
             createSequenceControls(attributes);
         })
 };
 
 function processData(data){
-    //empty array to hold attributes
     var attributes = [];
-
-    //properties of the first feature in the dataset
     var properties = data.features[0].properties;
-    //push each attribute name into attributes array
     for (var attribute in properties){
-        //only take attributes with population values
-        if (!(attribute in ['Country Name', 'Country Code'])){
+        if (!(attribute in ['Country Name', 'Country Code'])){ // Filter out the useful columns
             attributes.push(attribute);
         };
     };
-
-    //check result
-
     return attributes;
 };
 
