876515e597a753b00a8b3467a938dfbc158b90f1
QianhengZhang <hazeland3427@gmail.com>
2024-02-27 13:30:03 -0600

    Activity 6 finished

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 /* JavaScript functions by Qianheng Zhang, 2024 */
 var map
+var minValue
 
 function createMap(){
     //create the map
@@ -20,44 +21,174 @@ function createMap(){
     getData();
 };
 
-function onEachFeature(feature, layer) {
-    //no property named popupContent; instead, create html string with all properties
-    var popupContent = "";
-    if (feature.properties) {
-        //loop to add feature property names and values to html string
-        for (var property in feature.properties){
-            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+function calcMinValue(data){
+    //create empty array to store all data values
+    var allValues = [];
+    //loop through each city
+    for(var country of data.features){
+        //loop through each year
+        for(var year = 2011; year <= 2022; year+=1){
+              //get population for current year
+              var value = country.properties[String(year)];
+              //add value to array
+              allValues.push(value);
         }
-        layer.bindPopup(popupContent);
-    };
+    }
+    //get minimum value of our array
+    var minValue = Math.min(...allValues)
+
+    return minValue;
+}
+
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //constant factor adjusts symbol sizes evenly
+    var minRadius = 0.15;
+    //Flannery Appearance Compensation formula
+    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+
+    return radius;
 };
 
-function getData() {
-    fetch("data/WorldFoodExportShort.geojson")
+//Step 3: Add circle markers for point features to the map
+function createPropSymbols(data, attributes){
+
+    //Step 4: Determine which attribute to visualize with proportional symbols
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes)
+        }
+    }).addTo(map);
+};
+
+function pointToLayer(feature, latlng, attributes) {
+    //console.log(attributes)
+    var attribute = attributes[0];
+    console.log(attribute)
+    //create marker options
+    var geojsonMarkerOptions = {
+        fillColor: "#ff7800",
+        color: "#fff",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8,
+        radius: 8
+    };
+    var attValue = Number(feature.properties[attribute]);
+
+    geojsonMarkerOptions.radius = calcPropRadius(attValue);
+
+    var layer = L.circleMarker(latlng, geojsonMarkerOptions);
+    var year = attribute
+    var popupContent = "<p><b>Country:</b> " + feature.properties['Country Name'] + "</p>"
+    popupContent += "<p><b>Food Export in " + year + ":</b> " + feature.properties[attribute] + " % of Merchandise Exports</p>";
+
+            //bind the popup to the circle marker
+    layer.bindPopup(popupContent, {
+        offset: new L.Point(0,-geojsonMarkerOptions.radius)
+    });
+            //create circle markers
+    return layer
+}
+
+function createSequenceControls(attributes){
+    //create range input element (slider)
+    var slider = "<input class='range-slider' type='range'></input>";
+    document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
+    document.querySelector(".range-slider").max = 10;
+    document.querySelector(".range-slider").min = 0;
+    document.querySelector(".range-slider").value = 0;
+    document.querySelector(".range-slider").step = 1;
+
+    document.querySelector('#panel').insertAdjacentHTML('beforeend',"<img src='img/left.png' id='reverse' class='step'>")
+    document.querySelector('#panel').insertAdjacentHTML('beforeend',"<img src='img/right.png' id='forward' class='step'>")
+    document.querySelectorAll('.step').forEach(function(step){
+        step.addEventListener("click", function(){
+            var index = document.querySelector('.range-slider').value;
+
+            //Step 6: increment or decrement depending on button clicked
+            if (step.id == 'forward'){
+                index++;
+                //Step 7: if past the last attribute, wrap around to first attribute
+                index = index > 10 ? 0 : index;
+            } else if (step.id == 'reverse'){
+                index--;
+                //Step 7: if past the first attribute, wrap around to last attribute
+                index = index < 0 ? 10 : index;
+            };
+
+            //Step 8: update slider
+            document.querySelector('.range-slider').value = index;
+            console.log(index);
+            updatePropSymbols(attributes[index]);
+        })
+
+    })
+    document.querySelector('.range-slider').addEventListener('input', function(){
+        //Step 6: get the new index value
+        var index = this.value;
+        console.log(index)
+        updatePropSymbols(attributes[index]);
+    });
+};
+
+function updatePropSymbols(attribute){
+    map.eachLayer(function(layer){
+        if (layer.feature && layer.feature.properties[attribute]){
+            var props = layer.feature.properties;
+            console.log(attribute)
+            //update each feature's radius based on new attribute values
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
+
+            //add city to popup content string
+            var popupContent = "<p><b>Country:</b> " + props['Country Name'] + "</p>";
+
+            //add formatted attribute to panel content string
+            var year = attribute;
+            popupContent += "<p><b>Food Export in " + year + ":</b> " + props[attribute] + " % of Merchandise Exports</p>";
+
+            //update popup content
+            popup = layer.getPopup();
+            popup.setContent(popupContent).update();
+        };
+    });
+};
+
+//Step 2: Import GeoJSON data
+function getData(){
+    //load the data
+    fetch("data/FoodExport.geojson")
         .then(function(response){
             return response.json();
         })
         .then(function(json){
-          //create marker options
-        //     var geojsonMarkerOptions = {
-        //         radius: 8,
-        //         fillColor: "#ff7800",
-        //         color: "#000",
-        //         weight: 1,
-        //         opacity: 1,
-        //         fillOpacity: 0.8
-        //     };
-        //   //create a Leaflet GeoJSON layer and add it to the map
-        //     L.geoJson(json, {
-        //         pointToLayer: function (feature, latlng){
-        //             return L.circleMarker(latlng, geojsonMarkerOptions);
-        //         }
-        //     }).addTo(map);
-        L.geoJson(json, {
-            onEachFeature: onEachFeature
-        }).addTo(map);
-    });
-}
+            //calculate minimum data value
+            var attributes = processData(json);
+            minValue = calcMinValue(json);
+            //call function to create proportional symbols
+            createPropSymbols(json, attributes);
+            createSequenceControls(attributes);
+        })
+};
 
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
+
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with population values
+        if (!(attribute in ['Country Name', 'Country Code'])){
+            attributes.push(attribute);
+        };
+    };
+
+    //check result
+
+    return attributes;
+};
 
 document.addEventListener('DOMContentLoaded',createMap)
