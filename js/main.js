5792b0704d2b04d9d1154b47ab8f503670d794a0
QianhengZhang <hazeland3427@gmail.com>
2024-03-06 02:54:50 -0600

    Lab 1 completed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* JavaScript functions by Qianheng Zhang, 2024 */
-var map
-var minValue
+var map;
+var dataStats = {};
 
 function createMap(){
     //create the map
@@ -8,26 +8,24 @@ function createMap(){
         center: [20, 0],
         zoom: 2
     });
-
     //add OSM base tilelayer
-    L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}', {
-      minZoom: 1,
-      maxZoom: 16,
-      attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      ext: 'jpg'
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 3,
+	maxZoom: 16,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
     }).addTo(map);
-
     //call getData function
     getData();
 };
 
-function calcMinValue(data){
+function calcStats(data){
     //create empty array to store all data values
     var allValues = [];
     //loop through each city
     for(var country of data.features){
         //loop through each year
-        for(var year = 2011; year <= 2022; year+=1){
+        for(var year = 2010; year <= 2021; year+=1){
               //get population for current year
               var value = country.properties[String(year)];
               //add value to array
@@ -35,20 +33,25 @@ function calcMinValue(data){
         }
     }
     //get minimum value of our array
-    var minValue = Math.min(...allValues)
-
-    return minValue;
+    console.log(Math.min(...allValues))
+    dataStats.min = Math.min(...allValues);
+    dataStats.max = Math.max(...allValues);
+    var sum = allValues.reduce(function (a, b) {
+        return a + b;
+      });
+    dataStats.mean = sum / allValues.length;
 }
 
 function calcPropRadius(attValue) {
-     //0.15 for my data
-    var minRadius = 0.15;
+     //15 for my data
+    var minRadius = 15;
     //Flannery Appearance Compensation formula
-    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+    var radius = 1.0083 * Math.pow(attValue/dataStats.min,0.5715) * minRadius
 
     return radius;
 };
 
+// Add proportional symbols to the map
 function createPropSymbols(data, attributes){
     L.geoJson(data, {
         pointToLayer: function(feature, latlng){
@@ -59,9 +62,9 @@ function createPropSymbols(data, attributes){
 
 function pointToLayer(feature, latlng, attributes) {
     var attribute = attributes[0];
-    console.log(attribute)
+    //Define the proportional symbol
     var geojsonMarkerOptions = {
-        fillColor: "#ff7800",
+        fillColor: "#9BB0C1",
         color: "#fff",
         weight: 1,
         opacity: 1,
@@ -69,12 +72,13 @@ function pointToLayer(feature, latlng, attributes) {
         radius: 8
     };
     var attValue = Number(feature.properties[attribute]);
-
+    // Calculate the radius
     geojsonMarkerOptions.radius = calcPropRadius(attValue);
     var layer = L.circleMarker(latlng, geojsonMarkerOptions);
     var year = attribute;
+    // Add information to popup window
     var popupContent = "<p><b>Country:</b> " + feature.properties['Country Name'] + "</p>"
-    popupContent += "<p><b>Food Export in " + year + ":</b> " + feature.properties[attribute] + " % of Merchandise Exports</p>";
+    popupContent += "<p><b>Birth Rate in " + year + ":</b> " + feature.properties[attribute] + " of 1000 people</p>";
     layer.bindPopup(popupContent, {
         offset: new L.Point(0,-geojsonMarkerOptions.radius)
     });
@@ -82,6 +86,8 @@ function pointToLayer(feature, latlng, attributes) {
 }
 
 function createSequenceControls(attributes){
+    // panel
+    document.querySelector('#panel').insertAdjacentHTML('beforeend',"<img src='img/left-arrow.png' id='reverse' class='step'>")
     // slider
     var slider = "<input class='range-slider' type='range'></input>";
     document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
@@ -89,10 +95,10 @@ function createSequenceControls(attributes){
     document.querySelector(".range-slider").min = 0;
     document.querySelector(".range-slider").value = 0;
     document.querySelector(".range-slider").step = 1;
-    // panel
-    document.querySelector('#panel').insertAdjacentHTML('beforeend',"<img src='img/left.png' id='reverse' class='step'>")
-    document.querySelector('#panel').insertAdjacentHTML('beforeend',"<img src='img/right.png' id='forward' class='step'>")
-    // add function
+    document.querySelector('#panel').insertAdjacentHTML('beforeend',"<img src='img/right-arrow.png' id='forward' class='step'>")
+    document.querySelector('#panel').insertAdjacentHTML('beforeend',"<span id='year'>2010</span>")
+
+    // add functions for slider
     document.querySelectorAll('.step').forEach(function(step){
         step.addEventListener("click", function(){
             var index = document.querySelector('.range-slider').value;
@@ -104,18 +110,18 @@ function createSequenceControls(attributes){
                 index = index < 0 ? 10 : index;
             };
             document.querySelector('.range-slider').value = index;
-            console.log(index);
+            document.querySelector('#year').textContent = index + 2010;
             updatePropSymbols(attributes[index]);
         })
-
     })
     document.querySelector('.range-slider').addEventListener('input', function(){
         var index = this.value;
-        console.log(index)
+        document.querySelector('#year').textContent = parseInt(index) + 2010;
         updatePropSymbols(attributes[index]);
     });
 };
 
+// Update the proportional symbols as the year changes
 function updatePropSymbols(attribute){
     map.eachLayer(function(layer){
         if (layer.feature && layer.feature.properties[attribute]){
@@ -126,26 +132,15 @@ function updatePropSymbols(attribute){
             layer.setRadius(radius);
             var popupContent = "<p><b>Country:</b> " + props['Country Name'] + "</p>";
             var year = attribute;
-            popupContent += "<p><b>Food Export in " + year + ":</b> " + props[attribute] + " % of Merchandise Exports</p>";
+            popupContent += "<p><b>Birth Rate in " + year + ":</b> " + props[attribute] + " of 1000 people</p>";
             popup = layer.getPopup();
             popup.setContent(popupContent).update();
         };
     });
+    updateLegend(attribute); // update legend
 };
 
-function getData(){
-    fetch("data/FoodExport.geojson")
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(json){
-            var attributes = processData(json);
-            minValue = calcMinValue(json);
-            createPropSymbols(json, attributes);
-            createSequenceControls(attributes);
-        })
-};
-
+// Process Data
 function processData(data){
     var attributes = [];
     var properties = data.features[0].properties;
@@ -155,6 +150,143 @@ function processData(data){
         };
     };
     return attributes;
+};
+
+// Get max, mean, min of the birth rates
+function getCircleValues(attribute) {
+    //start with min at highest possible and max at lowest possible number
+    var min = Infinity,
+      max = -Infinity;
+  
+    map.eachLayer(function (layer) {
+      //get the attribute value
+      if (layer.feature) {
+        var attributeValue = Number(layer.feature.properties[attribute]);
+  
+        //test for min
+        if (attributeValue < min) {
+          min = attributeValue;
+        }
+  
+        //test for max
+        if (attributeValue > max) {
+          max = attributeValue;
+        }
+      }
+    });
+  
+    //set mean
+    var mean = (max + min) / 2;
+  
+    //return values as an object
+    return {
+      max: max,
+      mean: mean,
+      min: min,
+    };
+}
+
+// Create the initial legend in 2010
+function createLegend(attributes) {
+    var LegendControl = L.Control.extend({
+      options: {
+        position: "bottomleft",
+      },
+  
+      onAdd: function () {
+        // create the control container with a particular class name
+        var container = L.DomUtil.create("div", "legend-control-container");
+  
+        container.innerHTML = '<p class="temporalLegend">Birth Rate in <span class="year">2010</span></p>';
+  
+        //Step 1: start attribute legend svg string
+        var svg = '<svg id="attribute-legend" width="300px" height="140px">';
+  
+        //array of circle names to base loop on
+        var circles = ["max", "mean", "min"];
+  
+        //Step 2: loop to add each circle and text to svg string
+        for (var i = 0; i < circles.length; i++) {
+          //calculate r and cy
+          var radius = calcPropRadius(dataStats[circles[i]]);
+          console.log(radius);
+          var cy = 120 - radius;
+          console.log(cy);
+  
+          //circle string
+          svg +=
+            '<circle class="legend-circle" id="' +
+            circles[i] +
+            '" r="' +
+            radius +
+            '"cy="' +
+            cy +
+            '" fill="#9BB0C1" fill-opacity="0.8" stroke="#000000" cx="60"/>';
+  
+          //evenly space out labels
+          var textY = i * 30 + 55;
+  
+          //text string
+          svg +=
+            '<text id="' +
+            circles[i] +
+            '-text" x="120" y="' +
+            textY +
+            '">' +
+            Math.round(dataStats[circles[i]] * 100) / 100 +
+            " out of 1000" +
+            "</text>";
+        }
+  
+        //close svg string
+        svg += "</svg>";
+  
+        //add attribute legend svg to container
+        container.insertAdjacentHTML('beforeend',svg);
+  
+        return container;
+      },
+    });
+  
+    map.addControl(new LegendControl());
+  }
+
+  // Update the legend over the time
+function updateLegend(attribute) {
+    //create content for legend
+    var year = attribute;
+    //replace legend content
+    document.querySelector("span.year").innerHTML = year;
+  
+    //get the max, mean, and min values as an object
+    var circleValues = getCircleValues(attribute);
+  
+    for (var key in circleValues) {
+      //get the radius
+      var radius = calcPropRadius(circleValues[key]);
+  
+      document.querySelector("#" + key).setAttribute("cy", 120 - radius);
+      document.querySelector("#" + key).setAttribute("r", radius)
+  
+      document.querySelector("#" + key + "-text").textContent = Math.round(circleValues[key] * 100) / 100 + " out of 1000";
+
+    }
+}
+  
+// Load birth rate data
+function getData(){
+    fetch("data/topBirthRate.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            var attributes = processData(json); // load data
+            calcStats(json); // calculate max, mean, min of the data
+            // initialize the symbols, sliders, and legend
+            createPropSymbols(json, attributes);
+            createSequenceControls(attributes);
+            createLegend(attributes);
+        })
 };
 
 document.addEventListener('DOMContentLoaded',createMap)
